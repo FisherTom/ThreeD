42c20df661ca8107f215e0dbe3657a0f78b6b2b1
Tom Fisher <thomasdenisfisher@gmail.com>
2017-06-02 12:10:10 +0100

    Sandbox threejs

--- src/app.py
+++ src/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
      return render_template('index.html')
 
+@app.route('/three')
+def three():
+     return render_template('three.html')
+
 if __name__ == "__main__":
     app.run()
